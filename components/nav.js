a461df38965b25f693ce771b7e7bcda7dd2eb192
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2020-06-14 10:21:19 +0430

    Update: Set NavHeight, Set active link class

--- components/nav.js
+++ components/nav.js
@@ -1,28 +1,42 @@
-const {useState, useEffect} = React
+const {useState, useEffect, useContext} = React
 const {useLocation, Link} = ReactRouterDOM
 
 const Nav = () => {
 	const [toggled, setToggled] = useState(false)
+	
+	const mainContext = useContext(MainContext)
+	const location = useLocation()
+	
+	const changeNavHeight = () => {
+		mainContext
+			.setNavHeight(
+				window
+					.getComputedStyle(_navWrapper, null)
+					.getPropertyValue('height')
+					.split('px')[0]
+			)
+	}
+	
+	var _navWrapper
+	useEffect(() => changeNavHeight())
+	window.addEventListener('resize', changeNavHeight, false)
 	return(
-	<MainContext.Consumer>
-	{context =>
-	<div dir="rtl" className={`nav ${toggled ? 'toggled' : ''}`} style={{backgroundColor: context.theme.primaryBackgroundColor}}>
+	<div dir="rtl" className={`nav ${toggled ? 'toggled' : ''}`} style={{backgroundColor: mainContext.theme.primaryBackgroundColor}} ref={_nav => _navWrapper = _nav}>
 		<div className="nav__wrapper">
-			<div className="nav-brand__wrapper"><h1 className="nav-brand__h1" style={{color: context.theme.primaryFontColor}}>بست بوك</h1></div>
+			<Link to="/" style={{textDecoration: 'none'}}><div className="nav-brand__wrapper"><h1 className="nav-brand__h1" style={{color: mainContext.theme.primaryFontColor}}>بست بوک</h1></div>
+			</Link>
 			<div className="nav-hamburger">
-				<HamburgerButton lineColor={context.theme.primaryFontColor} toggled={toggled} toggle={setToggled}/>
+				<HamburgerButton lineColor={mainContext.theme.primaryFontColor} toggled={toggled} toggle={setToggled}/>
 			</div>
 		</div>
 		<div className={`nav-items__wrapper ${toggled ? 'toggled' : ''}`}>
-			<div><Link to="/" style={{textDecoration: 'none'}}><span className='link-item' style={{color: context.theme.primaryFontColor}}>خانه</span></Link></div>
-			<div><Link to="about-us" style={{textDecoration: 'none'}}><span className='link-item' style={{color: context.theme.primaryFontColor}}>درباره</span></Link></div>
-			<div><Link to="contact" style={{textDecoration: 'none'}}><span className='link-item' style={{color: context.theme.primaryFontColor}}>تماس با ما</span></Link></div>
+			<div><Link exact to="/" style={{textDecoration: 'none'}}><span className={`link-item ${location.pathname == '/' ? 'active' : ''}`} style={{color: mainContext.theme.primaryFontColor, borderBottomColor: mainContext.theme.primaryFontColor}}>خانه</span></Link></div>
+			<div><Link to="/about-us" style={{textDecoration: 'none'}}><span className={`link-item ${location.pathname == '/about-us' ? 'active' : ''}`} style={{color: mainContext.theme.primaryFontColor, borderBottomColor: mainContext.theme.primaryFontColor}}>درباره</span></Link></div>
+			<div><Link to="/contact" style={{textDecoration: 'none'}}><span className={`link-item ${location.pathname == '/contact' ? 'active' : ''}`} style={{color: mainContext.theme.primaryFontColor, borderBottomColor: mainContext.theme.primaryFontColor}}>تماس با ما</span></Link></div>
 			<Btn text='ورود' navButton outline/>
-			<Btn text='عضويت' navButton />
+			<Btn text='عضویت' navButton />
 			<ThemeSwitch />
 		</div>
 	</div>
-	}
-	</MainContext.Consumer>
 	)
 }
