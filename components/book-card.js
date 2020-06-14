210aa0615cc0464fab7516144a99a27904fba9bc
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2020-06-14 10:07:10 +0430

    Update: Major update for v0.2.1
    -Add link to book names
    -Update: Changed close-button place in layout and DOM
    -Add: Function to call TransitionManager for transitioning to book page

--- components/book-card.js
+++ components/book-card.js
@@ -1,13 +1,13 @@
-const {useContext, useState} = React
-const {useHistory, useLocation} = ReactRouterDOM
+const {useContext, useState, useRef} = React
+const {useHistory, useLocation, Link} = ReactRouterDOM
 
-const BookCard = ({view, book, information, onClickFn}) => {
+const BookCard = ({view, book, information, onClickFn, closeCallback}) => {
 	const {id, name, writer, translator, price, src} = book
 	const desc = book.desc || null
 	
 	var history = useHistory()
 	const location = useLocation()
-	
+
 	const context = useContext(MainContext)
 	
 	const [openTab, setOpenTab] = useState(0)
@@ -27,39 +27,56 @@ const BookCard = ({view, book, information, onClickFn}) => {
 		onClickFn('')
 	}
 	
-	const redirectToBookPage = (e) => {
-		e.preventDefault()
-		history.push({pathname: `/book/${id}`, state:{prevPath: location.pathname}})
+	const redirectToBookPage = () => {
+		history.push({pathname: `/book/${id}`, state:{prevPath: location.pathname, redirect: 'FORWARD'}})
 	}
 	
 	const goToPrevPath = (e) => {
 		e.preventDefault()
-		history.push({pathname: location.state.prevPath, state:{prevPath: location.pathname}})
+		if (closeCallback !== undefined) {
+			closeCallback().then(res => {
+				history.push({pathname: location.state.prevPath, state:{prevPath: location.pathname, redirect: 'BACKWARD'}})
+			})
+		} else {
+			history.push({pathname: location.state.prevPath, state:{prevPath: location.pathname, redirect: 'BACKWARD'}})
+		}
+	}
+	
+	const transitionToBook = (e) => {
+		e.preventDefault()
+		if(!information) {
+			context.endCompMockUp.callback().then(res => {
+				redirectToBookPage()
+			})
+		}
 	}
 	
 	const handleTabClick = (idx) => setOpenTab(idx)
 	
 	return (
+		<React.Fragment>
+			{information && location.state !== undefined && location.state.prevPath !== undefined && location.state.redirect !== undefined &&
+				<div dir='ltr' className="book-close-btn__wrapper">
+					<CloseBtn toggled onClickFn={goToPrevPath} />
+				</div>
+			}
 		<div className={`book-card__wrapper ${view}`}>
-			<div className="book-card__cover" style={{backgroundImage: `url("https://raw.githubusercontent.com/Alimobasheri/best-book/master/assets/${src}")`, borderColor: context.theme.bookCoverBorderColor}} onClick={redirectToBookPage}>
+			<div className={`book-card__cover ${!information ? 'fade' : ''}`} style={{backgroundImage: `url("assets/${src}")`, borderColor: context.theme.bookCoverBorderColor}} onClick={transitionToBook}>
 			</div>
 			<div className="book-card__text">
 				<div>
-					<div className="book-card__name" style={{color: context.theme.primaryFontColor}}>{name}</div>
+					<Link to={{pathname: `/book/${id}`, state: {prevPath: location.pathname, redirect: 'FORWARD'}}} style={{textDecoration:'none', color: context.theme.primaryFontColor}}><div className="book-card__name" style={{color: context.theme.primaryFontColor}} onClick={transitionToBook}>{name}</div></Link>
 					<div className="book-card__writer" style={{color: context.theme.secondaryFontColor}}>{writer}{translator && translator != 'false' && <span className="book-card__translator" style={{color: context.theme.secondaryFontColor}}> \ {translator}</span>}</div>
 				</div>
 				<div>
-					<Btn text={`${toFarsiDigits(price)} تومان`} outline />
+					<Btn text={`${toFarsiDigits(price)} ØªÙˆÙ…Ø§Ù†`} outline />
 				</div>
 			</div>
 			{information && 
 				<React.Fragment>
-					<div className="book-close-btn__wrapper">
-						<CloseBtn toggled onClickFn={goToPrevPath} />
-					</div>
 					<div className="desc-comments__wrapper">
 						<div className="tab-btns__wrapper">
-							<TabBtn titles={['توضيحات', 'نظرات']} onClickFn={handleTabClick} activeIdx={openTab}/>
+							<TabBtn titles={['ØªÙˆØ¶ÙŠØ­Ø§Øª', 'Ù†Ø¸Ø±Ø§Øª']} onClickFn={handleTabClick} activeIdx={openTab}/>
 						</div>
 						{desc !== null && desc.split("\n\n").map((para, idx) => 
 							<p key={idx} className="desc-comments__wrapper__desc__text" style={{color: context.theme.primaryFontColor}}>
@@ -70,5 +87,6 @@ const BookCard = ({view, book, information, onClickFn}) => {
 				</React.Fragment>
 			}
 		</div>
+		</React.Fragment>
 	)
 }
