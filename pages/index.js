59166f21cb4d1cfdce6d2e53b5ed32629db39bbc
Alimobasheri <a.mobasheri@icloud.com>
2020-10-09 20:00:32 +0330

    replace Home component code

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import MainContext from '../contexts/main-context'
+
+import List from '../components/list'
+import SearchBar from '../components/search-bar'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<MainContext.Consumer>
+		{context => {
+			const {books, homeLists, searchText, selectedBook} = context
+			
+			const homeListsEls =
+				homeLists.lists["HOME_LISTS"] && homeLists.lists["HOME_LISTS"].map((name, i) =>
+					<List key={i} view={'row'} title={homeLists.lists[name].title} books={[...books].filter(book => homeLists.lists[name].books_ids.includes(book.id))} />
+				)
+			
+			const homePage = 
+				selectedBook != '' ?
+					<List view={'column'} title={''} books={[...books].filter(book => book.id == selectedBook)} information /> :
+					homeListsEls
+			
+			const resultsList = searchText == '' ? 
+				[] : 
+				<List view={'column'} className="results-list" books={[...books].filter(book => book.name.includes(searchText) || book.writer.includes(searchText) || book.translator.includes(searchText))} />
+			return(
+				<div dir="rtl" className='home__wrapper'>
+					<div className='home__search-bar'><SearchBar responsive={true}/></div>
+					{searchText == '' ? 
+						homePage : 
+						resultsList
+					}
+				</div>
+			)
+		}
+		}
+		</MainContext.Consumer>
+	)
 }
