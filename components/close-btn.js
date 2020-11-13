eaec0c42c609581b540ba57776e89b8346bbd5a5
Alimobasheri <a.mobasheri@icloud.com>
2020-11-13 19:06:40 +0330

    load times icon instead of span

--- components/close-btn.js
+++ components/close-btn.js
@@ -1,15 +1,28 @@
 import {useContext} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
 
 import MainContext from '../contexts/main-context'
 
 export default function CloseBtn ({toggled, onClickFn}) {
 	const mainContext = useContext(MainContext)
-	const {primaryFontColor} = mainContext.theme
+	const {primaryFontColor, secondaryFontColor} = mainContext.theme
 	const lineStyle = {backgroundColor: primaryFontColor}
 	return (
-		<button className={`close-btn ${toggled ? 'toggled' : ''}`} onClick={onClickFn}>
-			<span className="close-btn__top-line" style={lineStyle}></span>
-			<span className="close-btn__bottom-line" style={lineStyle}></span>
+		<button 
+		className={`close-btn ${toggled ? 'toggled' : ''}`}
+		onClick={onClickFn}>
+			<FontAwesomeIcon icon={faTimes} />
+			<style jsx>{`
+				.close-btn {
+					color: ${secondaryFontColor};
+				}
+
+				.close-btn:hover {
+					cursor: pointer;
+					color: ${primaryFontColor}
+				}
+			`}</style>
 		</button>
 	)
 }
