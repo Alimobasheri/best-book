3e364de2c4e0a9d1c9829430b216d851b48d871d
Alimobasheri <a.mobasheri@icloud.com>
2020-11-17 00:32:37 +0330

    new list stylings with icon

--- components/list.js
+++ components/list.js
@@ -3,8 +3,24 @@ import {useState, useEffect} from 'react'
 import MainContext from '../contexts/main-context'
 import BookCard from './book-card'
 
-export default function List ({view, title, books, information, closeCallback}) {
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faLayerGroup, faPlus, faLandmark} from '@fortawesome/free-solid-svg-icons'
+
+export default function List ({view, title, iconType, books, information, closeCallback}) {
 	const [scroll, setScroll] = useState(false)
+
+	const SetListIcon = (iconType) => {
+		switch(iconType) {
+			case 'top-sale':
+				return <FontAwesomeIcon icon={faLayerGroup} />
+			case 'newest':
+				return <FontAwesomeIcon icon={faPlus} />
+			case 'history':
+				return <FontAwesomeIcon icon={faLandmark} />
+			default:
+				return null
+		}
+	}
 	useEffect(() => {
 		if(books.length > 0) setTimeout(() => setScroll(true), 700)
 	})
@@ -12,7 +28,9 @@ export default function List ({view, title, books, information, closeCallback}) 
 		<MainContext.Consumer>
 		{context =>
 		<div className={`list ${information && 'full-width'}`}>
-			<h1 className="list__title bb-typography__title" style={{color: context.theme.primaryFontColor}}>{title}</h1>
+			<h1 className="list__title bb-typography__title">
+				{SetListIcon(iconType)}  {title}
+			</h1>
 			{books && books.length > 0 ?
 				<div className={`list__wrapper list__wrapper__${view == 'column' ? 'column' : 'row'} ${information && 'full-width'} ${scroll ? 'webkit-scroll' : ''}`} >
 					{
@@ -30,6 +48,10 @@ export default function List ({view, title, books, information, closeCallback}) 
 					.list {
 						background-color: transparent;
 					}
+
+					.list__title {
+						color: ${context.theme.primaryFontColor}
+					}
 					@media only screen and (min-width: 768px) {
 						.list {
 							background-color: ${context.theme.secondaryBackgroundColor}
@@ -38,6 +60,19 @@ export default function List ({view, title, books, information, closeCallback}) 
 					@media only screen and (min-width: 1200px) and (min-height: 300px) {
 						.list.full-width {
 							background-color: transparent;
+						}
+						.list {
+							padding: 0;
+						}
+						.list__title {
+							color: ${context.theme.primaryBackgroundColor};
+							background-color: ${context.theme.primaryFontColor};
+							border-top-left-radius: 20px;
+							border-top-right-radius: 20px;
+						}
+						.list.full-width > .list__title {
+							background-color: transparent;
+							border-radius: 0;
 						}
 					}
 				`}
