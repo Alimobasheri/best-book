1830c2cb50755ffdb05d44c309cba2e851bcbb2a
Alimobasheri <a.mobasheri@icloud.com>
2020-11-25 10:46:08 +0330

    page for search results

---
+++ pages/search/[search_query].js
@@ -0,0 +1,38 @@
+import {useContext} from 'react'
+import {useRouter} from 'next/router'
+
+import MainContext from '../../contexts/main-context'
+
+import List from '../../components/list'
+
+export default function Search() {
+    const mainContext = useContext(MainContext)
+    const {searchText, books} = mainContext
+
+    const router = useRouter()
+    const {search_query} = router.query
+    return (
+        <div className='search-page__wrapper'>
+            {search_query && search_query !== '' ?
+                <List 
+                view={'column'}
+                title={`نتایج جستجو برای '${searchText}'`}
+                className="results-list"
+                books={[...books].filter(book => 
+                    book.name.includes(search_query) || book.writer.includes(search_query) || book.translator.includes(search_query)
+                )} /> :
+
+                <List
+                view='column'
+                title='نتایج جستجو'
+                className='results-list'
+                books={[]}/>
+            }
+            <style jsx>{`
+                .search-page__wrapper {
+                    padding: 3vh 2vw;
+                }
+            `}</style>
+        </div>
+    )
+}
