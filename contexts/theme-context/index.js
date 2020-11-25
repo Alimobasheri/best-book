3991b3283fbeb8c67399eb37c8d0eefbd866ef6b
Alimobasheri <a.mobasheri@icloud.com>
2020-11-25 10:43:48 +0330

    create default export for theme context

---
+++ contexts/theme-context/index.js
@@ -0,0 +1,9 @@
+import ThemeContext from './theme-context'
+import ThemeProvider from './theme-provider'
+import withTheme from './with-theme'
+
+export {
+    ThemeContext,
+    ThemeProvider,
+    withTheme
+}
