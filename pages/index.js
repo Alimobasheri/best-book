f2ef6ceac0f9cfa59d029cac3be9e4c53bd190bf
Alimobasheri <a.mobasheri@icloud.com>
2020-11-25 10:45:09 +0330

    carousel photos with background, searchBar replace

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,30 @@ import HEAD from 'next/head'
 import MainContext from '../contexts/main-context'
 
 import List from '../components/list'
-import SearchBar from '../components/search-bar'
 import Carousel from '../components/carousel/carousel'
 import AdvertisementCarouselItems from '../components/advertisement-carousel'
 
 export default function Home() {
 	const mainContext = useContext(MainContext)
-	const {books, homeLists, searchText, selectedBook} = mainContext
+	const {books, homeLists, selectedBook} = mainContext
 
 	const carouselNewItems = [
 		{
 			title: 'کتاب بخوانید',
 			subtitle: 'جدیدترین کتابهای روز را در پایگاه بست بوک خواهید یافت.',
+			backgroundColor: '#706fd3',
 			src: './assets/carousel-photos/mock1.jpg'
 		},
 		{
 			title: 'آنچه روحت می پسندد بیاب',
 			subtitle: 'با جسنجو در دسته بندیها، کتابهایی را با هر موضوع و پسندی بیابید.',
+			backgroundColor: '#b33939',
 			src: './assets/carousel-photos/mock2.jpg'
 		},
 		{
 			title: 'کتاب های خردورزانه',
 			subtitle: 'از برادران کارامازوف تا زنان زیرک و هزاران کتاب خردمندانه ی دیگر که در پایگاه عظیم بست بوک یافت می شوند.',
+			backgroundColor: '#cd6133',
 			src: './assets/carousel-photos/mock3.jpg'
 		}
 	]
@@ -35,17 +37,21 @@ export default function Home() {
 
 	const homeListsEls =
 		homeLists.lists["HOME_LISTS"] && homeLists.lists["HOME_LISTS"].map((name, i) =>
-			<List key={i} view={'row'} title={homeLists.lists[name].title} books={[...books].filter(book => homeLists.lists[name].books_ids.includes(book.id))} />
+			<List 
+			key={i} 
+			view={'row'} 
+			title={homeLists.lists[name].title}
+			iconType={homeLists.lists[name].icon}
+			books={[...books].filter(book => homeLists.lists[name].books_ids.includes(book.id))} />
 		)
 	
 	const homePage = 
 		selectedBook != '' ?
 			<List view={'column'} title={''} books={[...books].filter(book => book.id == selectedBook)} information /> :
 			<React.Fragment>
-				<h1 
-				className="page-header">
-					کتابگردی
-				</h1>
+				<div 
+				className="home-hero">
+				</div>
 				<div className='home__carousel__wrapper'>
 					<Carousel
 					items={advertiseItems}
@@ -58,22 +64,13 @@ export default function Home() {
 					}
 				`}</style>
 			</React.Fragment>
-	
-	const resultsList = searchText == '' ? 
-		[] : 
-		<List view={'column'} className="results-list" books={[...books].filter(book => book.name.includes(searchText) || book.writer.includes(searchText) || book.translator.includes(searchText))} />
-	
 	return(
 		<div dir="rtl" className='home__wrapper'>
         	<HEAD>
         		<title>بست بوک | خانه</title>
         	</HEAD>
-			<div className='home__search-bar'>
-				<SearchBar responsive={true}/>
-			</div>
-			{searchText == '' ? 
-				homePage : 
-				resultsList
+			{
+				homePage
 			}
 		</div>
 	)
