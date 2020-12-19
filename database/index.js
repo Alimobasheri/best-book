22fb28f3991eb4aa5dda2c9604c1ab7e2611e6b9
Alimobasheri <a.mobasheri@icloud.com>
2020-12-19 14:10:06 +0330

    Create: export all db funcs here

---
+++ database/index.js
@@ -0,0 +1,25 @@
+import connect from './connect/connect'
+
+import registerUserProfile from './users/register-user-profile'
+import findUserByEmail from './users/find-user-by-email'
+
+import findBooks from './books/find-books'
+import registerBook from './books/register-book'
+
+import findAuthor from './authors/find-author'
+import registerAuthor from './authors/register-author'
+
+import addGenre from './genres/add-genre'
+import findGenre from './genres/find-genre'
+
+export {
+    connect,
+    registerUserProfile,
+    findUserByEmail,
+    findBooks,
+    registerBook,
+    findAuthor,
+    registerAuthor,
+    addGenre,
+    findGenre
+}
